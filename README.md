203fe42d702bd497fc9baa38a76e959efaa96a56
pdn <pnathan@vandals.uidaho.edu>
2014-03-18 00:03:29 -0700

    Added test "groups".
    Typically in jUnit or other common OO unit test frameworks, tests are
    conceived of as methods of classes which are iterated
    over. Unfortunately, this pattern simply does not apply in Common Lisp
    very well, as methods are free and unattached to classes.
    
    Taking fiveam's lead, I create mechanism to "group" tests together in a
    simple fashion and provide an API for controlling which group a test is
    designated to live under.

--- README.md
+++ README.md
@@ -13,6 +13,60 @@ checker is a library for simple and reasonable people.
 
 Its API isn't finalized, but should be roughly stable.
 
+API
+===
+
+#:check
+
+`check` is the core operation of `checker`. Check simply verifies the
+truthiness & non-condition-throwing-ness of its `test-form`. It has
+two optional positional parameters following `test-form`: `name`, a
+human readable test name and `identifier`, a machine-readable
+identifier. These will be generated if not supplied.
+
+Check will enter its results in the current `*test-group*` field of
+the `*test-groups-table*`. However, the overlaying macro and CLOS
+machinery should render that nearly invisible to the user.
+
+Check should not be placed in the middle of tight loops, as it
+generates a surprising amount of logic involving recording data
+against dynamic variables.
+
+#:*verbosity*
+
+`*verbosity*` controls how spammy `check` is to
+`*standard-output*`. Valid values are [0-3] on the integers.
+
+#:run-checker-tests
+
+`run-checker-tests` is a CLOS method whose *primary* method should be
+overridden for your package's specifier. This specifier will be used
+in the `*test-groups-table*` as the `*test-group*`.
+
+#:render-checker-results
+
+`render-checker-results` should be overridden using a specializer
+bound to `*expected-stream*` if you need to write your own result
+renderer/analysis tool.
+
+#:*expected-stream*
+
+See above.
+
+#:with-test-group
+
+`with-test-group` is a macro that rebinds test group name lexically
+and returns total numbers of runs and successes as values.
+
+#:*test-group*
+
+`*test-group*` is the current key in the test-groups-table.
+
+#:*test-groups-table*
+
+`*test-groups-table*` is the primary entry point for all data recorded
+in the current test session.
+
 
  Examples
 ===
@@ -62,8 +116,12 @@ Anyway, for the above example we have the following definitions, roughly:
          ;; .. we continue on with more checks...
          )
 
-       (defmethod checker:run-checker-tests ()
+       (defmethod checker:run-checker-tests ((specializer (eql :clink)))
           (check-headers-test))
 
        (defun run-tests ()
-          (checker:run-checker-tests))
+          (checker:run-checker-tests :clink))
+
+
+Observe that checker:run-checker-tests requires a specializer - this
+is to distinguish your fine tests from others extant in your system.
--- checker.lisp
+++ checker.lisp
@@ -1,88 +1,134 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;; checker.lisp
+;;;; licence: llgpl
+;;;; author: paul nathan
+;;;;
+;;;; a simple unit test framework without magic
+
 (defpackage :checker
   (:use :cl)
   (:export #:check
-           #:run-checker-tests
-           #:*expected-stream*
            #:*verbosity*
-           #:render-checker-results))
+           #:run-checker-tests
+
+           #:render-checker-results
+           #:*expected-stream*
+
+           #:with-test-group
+           #:*test-group*
+           #:*test-groups-table*))
 
 (in-package :checker)
 
+;;; defparameters are chosen to be dynamically overloaded at will by
+;;; the end user to ensure clever and interesting capabilities.
 (defparameter *verbosity* 1)
 (defparameter *expected-stream* t)
+(defparameter *test-group* 'default)
+(defparameter *test-groups-table* (make-hash-table))
 
-(defparameter *tests-run* 0)
-(defparameter *tests-failed* 0)
-(defparameter *erroroneous-tests* nil)
+(defmacro with-test-group ((test-suite &optional (verbosity *verbosity*)) &body body)
+  "Specifies the test group to execute under, along with the possible
+verbosity of this lexical body. Test-suite in *test-groups-table* will
+be overwritten."
+  `(let ((*test-group* ,test-suite)
+         (*verbosity* ,verbosity))
+     (remhash ,test-suite *test-groups-table*)
+     (progn
+       ,@body
+       (let ((results (gethash ,test-suite *test-groups-table*)))
+         (values
+          (length results)
+          (count-if #'(lambda (result) (cdr (assoc :success result))) results))))))
 
-(defmacro check (test-form &optional name identifier)
+(defmacro check (test-form &optional (name :default) identifier)
   "Verify that the expansion and evaluation of test-form results in
 something truthy. Falsity or a condition raised will result in test
 failure. Test-form will be expanded exactly once. "
-  ;; TODO: restructure internals...
-  (declare (ignore name)
-           (ignore identifier))
 
   (let ((form-data (loop for element in test-form collect `',element))
-        (form (gensym)))
+        (form (gensym))
+        (success (gensym))
+        (identifier-sym (gensym "I")))
 
-   `(flet ((test-failure (form)
-             (cond ((and (< *verbosity* 3)
-                         (> *verbosity* 0))
-                    (format t "~&Test failure: ~a~%" form))
-                   ((> *verbosity* 3)
-                    (format t "failed~&"))
-                   ((= *verbosity* 0)
-                    t))
-             (push form *erroroneous-tests*)
-             (incf *tests-failed*))
+   `(let ((,form (list ,@form-data))
+          (,identifier-sym (if ,identifier ,identifier (gensym)))
+          (,success nil))
 
-           (test-success (form)
-             (declare (ignore form))
-             (if (>= *verbosity* 3)
-                 (format t "succeeded~&"))))
+      (flet ((test-failure (form)
+               (cond ((and (< *verbosity* 3)
+                           (> *verbosity* 0))
+                      (format t "~&Test failure: ~a~%" form))
+                     ((>= *verbosity* 3)
+                      (format t "failed~&"))
+                     ((= *verbosity* 0)
+                      t))
+               (setf ,success nil))
 
+             (test-success (form)
+               (declare (ignore form))
+               (if (>= *verbosity* 3)
+                   (format t "succeeded~&"))
+               (setf ,success t)))
 
-      (let ((,form (list ,@form-data)))
+        ;; logging
         (cond ((and (>= *verbosity* 1) (< *verbosity* 3))
                (format t "."))
               ((>= *verbosity* 3)
                (format t "~&Testing: ~a ... " ,form)))
 
         (handler-case
-           (cond
-             (,test-form
-              (test-success ,form))
-             (t
-              (test-failure ,form)))
-         (error (condition)
-           (test-failure ,form)
-           (values nil condition))))
-      (incf *tests-run*)
-      (values *tests-run* *tests-failed* *erroroneous-tests*))))
+            (cond
+              ;; actual execution of test-form
+              (,test-form
+               (test-success ,form))
+              (t
+               (test-failure ,form)))
+          (error (condition)
+            (test-failure ,form)
+            (values nil condition)))
 
+        ;; record execution
 
-(defgeneric run-checker-tests ()
-  :documentation "")
+        (let ((results (list (cons  :id ,identifier-sym)
+                             (cons :name ,name)
+                             (cons :form ,form)
+                             (cons :success ,success))))
+          (if (gethash *test-group* *test-groups-table*)
+              (push results
+                   (gethash *test-group* *test-groups-table*))
+              (setf (gethash *test-group* *test-groups-table*)
+                    (list results))))
 
-(defmethod run-checker-tests :before ()
-  (setf *tests-run* 0)
-  (setf *tests-failed* 0)
-  (setf *erroroneous-tests* nil))
+        (gethash *test-group* *test-groups-table*)))))
 
-(defmethod run-checker-tests :after ()
-  (let ((result `((:run ,*tests-run*)
-                  (:failed ,*tests-failed*)
-                  (:tests ,*erroroneous-tests*))))
-    (render-checker-results result *expected-stream*)))
+(defgeneric run-checker-tests (specializer)
+  (:documentation "Overload this method for your own package. Within
+  this method, call directly or indirectly the checker macro. For the
+  specializer, pass in your test suite name as an specializer. The
+  test results will be stored under this suite's name"))
+
+(defmethod run-checker-tests :before (specializer)
+  (format t "~&Using test group ~a~%" specializer))
+
+(defmethod run-checker-tests :around (specializer)
+  (with-test-group (specializer)
+    (call-next-method)))
+
+(defmethod run-checker-tests :after (specializer)
+  (render-checker-results (gethash specializer *test-groups-table*)
+                          *expected-stream*))
 
 (defgeneric render-checker-results (result stream)
   (:documentation "Kludge of the CLOS dispatch system - create your
   own 'stream' and you too can handle the results as you see
-  fit. Results is an alist with keys :run, :failed, and :tests. :run is number
-  of tests executed. :tests are the forms which failed"))
+  fit. Results is a list of alists. Keys
+  are :id (identifier), :name (human readable name), :form (form
+  used, and :success (the success or failure of the check)."))
 
 (defmethod render-checker-results (result (stream t))
   (when (> *verbosity* 0)
-    (format t "~&Tests run: ~a~%" (cadr (assoc :run result)))
-    (format t "~&Tests failed: ~a~%" (cadr (assoc :failed result)))))
+    (values
+     (format t "~&Tests run: ~a~%" (length result))
+     (format t "~&Tests succeeded: ~a~%"
+             (count-if #'(lambda (r) (cdr (assoc :success r))) result)))))
